b5f7c33b3b387b39183102b356c35ab8d99af458
VladyslavAslanov <vlad.aslanov@icloud.com>
2024-02-23 02:33:57 +0100

    Style: Scroll is disabled

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { FC } from "react"
 const App: FC = () => {
 	return (
 		<div className="flex justify-center items-center h-full flex-col gap-4">
-			<h1 className="font-bold text-6xl">Vladyslav Aslanov</h1>
-			<h2 className="text-3xl">Software Engineer</h2>
+			<h1 className="font-bold text-6xl text-center">Vladyslav Aslanov</h1>
+			<h2 className="text-3xl text-center">Software Engineer</h2>
 			<div className="flex gap-4">
 				<a href="https://docs.google.com/document/d/1fniXTjOW-U2C7ixGPAzgSBLZ3pgqBpNwuTQsXdTiDMs/edit?usp=sharing"
 					 target="_blank"
