c65bff697c6ccffd3616ff233848c1a57052716a
VladyslavAslanov <vlad.aslanov@icloud.com>
2024-02-23 02:00:52 +0100

    Feat: Add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import { FC } from "react"
 
 const App: FC = () => {
-	return <div />
+	return (
+		<div className="flex justify-center items-center h-full flex-col gap-4">
+			<h1 className="font-bold text-6xl">Vladyslav Aslanov</h1>
+			<h2 className="text-3xl">Software Engineer</h2>
+			<div className="flex gap-4">
+				<a href="https://docs.google.com/document/d/1fniXTjOW-U2C7ixGPAzgSBLZ3pgqBpNwuTQsXdTiDMs/edit?usp=sharing"
+					 target="_blank"
+					 className="cursor-pointer underline">Resume</a>
+				<a href="https://www.linkedin.com/in/vladyslav-aslanov/"
+					 target="_blank"
+					 className="cursor-pointer underline">LinkedIn</a>
+			</div>
+		</div>
+	)
 }
 
 export default App
